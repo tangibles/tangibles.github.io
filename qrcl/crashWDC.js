3212c6d07d5ceb640db189dd0287cc4a2e7a0a46
Carmel McGinley <carmel@quant.com.au>
2019-04-20 10:16:57 +1000

    Dev

--- qrcl/crashWDC.js
+++ qrcl/crashWDC.js
@@ -7,16 +7,22 @@
         dataType: tableau.dataTypeEnum.string
     }, {
       id: "Crash_Ref_Number",
-      alias: "place",
+      alias: "Crash ID",
       dataType: tableau.dataTypeEnum.string
      }, {
        id: "Crash_Severity",
-       alias: "mseconds",
+       alias: "Severity",
        dataType: tableau.dataTypeEnum.int
      }];
 
     schemaCallback([tableSchema]);
   };
+
+    var tableSchema = {
+      id: "Crash Data",
+      alias: "Road crashes in Queensland",
+      columns: cols
+    };
 
     myConnector.getData = function(table, doneCallback) {
         $.getJSON("https://data.qld.gov.au/api/action/datastore_search?resource_id=e88943c0-5968-4972-a15f-38e120d72ec0&limit=5", function(resp) {
@@ -27,12 +33,8 @@
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
                     "id": feat[i].id,
-                    "place": feat[i].properties.place,
-                    "time": feat[i].properties.time,
-                    "mag": feat[i].properties.mag,
-                    "tsunami": feat[i].properties.tsunami,
-                    "title": feat[i].properties.title,
-                    "location": feat[i].geometry
+                    "Crash_Ref_Number": feat[i].properties.Crash_Ref_Number,
+                    "Crash_Severity": feat[i].properties.Crash_Severity
                 });
             }
 
