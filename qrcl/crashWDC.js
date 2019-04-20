4aa85a622036e1479db7a27dc584b574629c9592
Carmel McGinley <carmel@quant.com.au>
2019-04-20 15:25:02 +1000

    Dev

--- qrcl/crashWDC.js
+++ qrcl/crashWDC.js
@@ -3,17 +3,20 @@
 
     myConnector.getSchema = function (schemaCallback) {
     var cols = [{
-        id: "id",
-        dataType: tableau.dataTypeEnum.string
-    }, {
       id: "Crash_Ref_Number",
-      alias: "Crash ID",
+      alias: "Crash_Ref_Number",
       dataType: tableau.dataTypeEnum.int
      }, {
        id: "Crash_Severity",
-       alias: "Severity",
+       alias: "Crash_Severity",
        dataType: tableau.dataTypeEnum.string
      }];
+
+     var tableSchema = {
+         id: "crashFeed",
+         alias: "Crash feed",
+         columns: cols
+     };
 
     schemaCallback([tableSchema]);
   };
@@ -21,16 +24,16 @@
 
 
     myConnector.getData = function(table, doneCallback) {
-        $.getJSON("https://data.qld.gov.au/api/action/datastore_search?resource_id=e88943c0-5968-4972-a15f-38e120d72ec0&limit=5", function(resp) {
-            var feat = resp.features,
+        $.getJSON("https://data.qld.gov.au/api/action/datastore_search?resource_id=e88943c0-5968-4972-a15f-38e120d72ec0",
+        function(resp) {
+            var feat = resp,
                 tableData = [];
 
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-                    "id": feat[i].id,
-                    "Crash ID": feat[i].properties.Crash_Ref_Number,
-                    "Severity": feat[i].properties.Crash_Severity
+                    "Crash_Ref_Number": feat[i].records.Crash_Ref_Number,
+                    "Crash_Severity": feat[i].records.Crash_Severity
                 });
             }
 
@@ -40,10 +43,13 @@
     };
 
     tableau.registerConnector(myConnector);
+
+    //event listener
     $(document).ready(function () {
       $("#submitButton").click(function () {
         tableau.connectionName = "Qld Road Crash Location Feed";
         tableau.submit();
     });
 });
+
 })();
