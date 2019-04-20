56ffdf7c5827a1ec50817c23149203c0269d7b2a
Carmel McGinley <carmel@quant.com.au>
2019-04-20 10:06:14 +1000

    S4

--- qrcl/crashWDC.js
+++ qrcl/crashWDC.js
@@ -16,7 +16,7 @@
      };
 
     schemaCallback([tableSchema]);
-    };
+  }];
 
     myConnector.getData = function(table, doneCallback) {
         $.getJSON("https://data.qld.gov.au/api/action/datastore_search?resource_id=e88943c0-5968-4972-a15f-38e120d72ec0&limit=5", function(resp) {
